bb252380bbad194c490563aaa6d03ec531e475eb
Yann Mazita <dev.mazita@gmail.com>
2024-04-19 11:23:26 +0200

    Added tracking of information type

--- front/src/stores/menu.ts
+++ front/src/stores/menu.ts
@@ -6,6 +6,14 @@ export const useMenuStore = defineStore('menu', () => {
     const codeChoice: Ref<boolean> = ref(false); // The user has chosen to input code received by email.
     const helpChoice: Ref<boolean> = ref(false); // The user has chosen to go to the help page.
     const currentPageTitle: Ref<string> = ref("Home"); // The title of the current page.
+    const informationType: Ref<string | null> = ref(null); // The type of information the user is inputting.
+
+    function setInformationTypeEmail(): void {
+        informationType.value = "email";
+    }
+    function setInformationTypeID(): void {
+        informationType.value = "id";
+    }
 
     function setInformationChoice(value: boolean): void {
         informationChoice.value = value;
@@ -38,6 +46,7 @@ export const useMenuStore = defineStore('menu', () => {
 
 
     return {
+        informationType,
         informationChoice,
         codeChoice,
         helpChoice,
@@ -45,6 +54,8 @@ export const useMenuStore = defineStore('menu', () => {
         currentPageTitle,
         setCodeChoice,
         setHelpChoice,
+        setInformationTypeID,
+        setInformationTypeEmail,
         resetChoices,
     }
 })
