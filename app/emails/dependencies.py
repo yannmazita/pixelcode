c2f190da43fd7779bb07c743e1ae4af4c48c2f05
Yann Mazita <dev.mazita@gmail.com>
2024-04-26 23:58:28 +0200

    Spun out SSL SMTP to separate class

--- app/emails/dependencies.py
+++ app/emails/dependencies.py
@@ -1,5 +1,10 @@
 from app.emails.services import EmailService
+from app.emails.services import EmailServiceSecure
 
 
 def get_email_service():
     return EmailService()
+
+
+def get_email_service_secure():
+    return EmailServiceSecure()
