fb2799a928e4f5203902abeed99926d024d9ff4e
Yann Mazita <dev.mazita@gmail.com>
2024-04-26 19:41:37 +0200

    Spun out oauth scope into separate variable

---
+++ app/auth/config.py
@@ -0,0 +1,14 @@
+OAUTH_SCOPES = {
+    "user.create": "The ability to create a new user.",
+    "user:own": "Read only access to the current user's information.",
+    "user:own.write": "The ability to change the current user's information.",
+    "user:others": "Read only access to other users' information.",
+    "user:others.write": "The ability to change other users' information.",
+    "employee.create": "The ability to create a new employee.",
+    "employee:own": "Read only access to the current employee's information.",
+    "employee:own.write": "The ability to change the current employee's information.",
+    "employee:others": "Read only access to other employees' information.",
+    "employee:others.write": "The ability to change other employees' information.",
+    "websockets": "Access to the websocket.",
+    "admin": "Full access to all information.",
+}
