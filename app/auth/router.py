9647f759c83e36f417b429140395edef5c57ea89
Yann Mazita <dev.mazita@gmail.com>
2024-04-26 23:47:11 +0200

    Updated register route

--- app/auth/router.py
+++ app/auth/router.py
@@ -5,9 +5,14 @@ from datetime import timedelta
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
 from dotenv import load_dotenv
+from sqlmodel import Session
 
+from app.auth.exceptions import username_already_exists
 from app.auth.services import authenticate_user, create_access_token
 from app.auth.models import Token
+from app.database import get_session
+from app.users.schemas import UserAttribute
+from app.users.services import UserService
 
 load_dotenv()
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
@@ -39,14 +44,12 @@ async def login_for_access_token(
 @router.post("/register", response_model=Token)
 async def register_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: Annotated[Session, Depends(get_session)],
 ):
-    user = authenticate_user(form_data.username, form_data.password)
-    if user is not None:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Username already exists",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    service = UserService(session)
+    if service.get_user_by_attribute(UserAttribute.USERNAME, form_data.username):
+        raise username_already_exists
+
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": form_data.username}, expires_delta=access_token_expires
