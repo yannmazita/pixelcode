0a37b1f021277fd369e2161c939178d9ead31db8
Yann Mazita <dev.mazita@gmail.com>
2024-04-29 13:44:37 +0200

    Updated inputs

--- front/src/components/AppLoginHero.vue
+++ front/src/components/AppLoginHero.vue
@@ -1,30 +1,22 @@
 <template>
-    <div class="hero min-h-screen bg-base-200">
+    <div class="hero min-h-screen">
         <div class="hero-content flex-col lg:flex-row-reverse">
             <div class="text-center lg:text-left">
                 <h1 class="text-5xl font-bold">Login now!</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-                    exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <p class="py-6">Connect to the administration dashboard.</p>
             </div>
             <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <form class="card-body">
+                <form class="card-body bg-base-200">
                     <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Email</span>
-                        </label>
-                        <input type="email" placeholder="email" class="input input-bordered" required />
+                        <AppInput v-model="username" placeholder="username" class="input input-bordered"></AppInput>
                     </div>
                     <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Password</span>
-                        </label>
-                        <input type="password" placeholder="password" class="input input-bordered" required />
-                        <label class="label">
-                            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
+                        <AppInput v-model="password" placeholder="password" class="input input-bordered"></AppInput>
                     </div>
                     <div class="form-control mt-6">
-                        <button class="btn btn-primary">Login</button>
+                        <AppButton :disabled="isSubmitting" type="submit" class="btn btn-primary text-sm">
+                            {{ 'Submit' }}
+                        </AppButton>
                     </div>
                 </form>
             </div>
@@ -33,4 +25,21 @@
 </template>
 
 <script setup lang="ts">
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/yup';
+import { object, string } from 'yup';
+import AppInput from '@/components/AppInput.vue'
+import AppButton from '@/components/AppButton.vue'
+
+const schema = toTypedSchema(
+    object({
+        username: string().required().min(1).max(255).default(''),
+        password: string().required().min(1).max(255).default(''),
+    }),
+);
+const { handleSubmit, isSubmitting, defineField } = useForm({
+    validationSchema: schema,
+});
+const [username] = defineField('username');
+const [password] = defineField('password');
 </script>
