4a1cb117355ae5fa479af18e3eb91cb54143cb8f
Yann Mazita <dev.mazita@gmail.com>
2024-05-05 12:53:58 +0200

    Responsive padding

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-12">
         <div class="col-span-12"></div>
         <NavBar class="col-span-12"></NavBar>
-        <main class="col-span-12 h-[calc(100vh-64px)]">
+        <main class="col-span-12 h-[calc(100vh-64px)] p-3.5 lg:p-6">
             <router-view></router-view>
         </main>
     </div>
