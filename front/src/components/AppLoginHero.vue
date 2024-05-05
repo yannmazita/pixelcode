3df4e4701ecd7c2ada764efa6293612528c0ee21
Yann Mazita <dev.mazita@gmail.com>
2024-05-05 22:22:29 +0200

    Width update

--- front/src/components/AppLoginHero.vue
+++ front/src/components/AppLoginHero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hero min-h-screen">
+    <div class="hero">
         <div class="hero-content flex-col lg:flex-row-reverse">
             <div class="text-center lg:text-left">
                 <h1 class="text-5xl font-bold">Login now!</h1>
