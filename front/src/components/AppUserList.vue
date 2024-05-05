7055fbdd93863968a318f508daf7d7cd66386f83
Yann Mazita <dev.mazita@gmail.com>
2024-05-05 17:58:45 +0200

    UX and responsiveness update

--- front/src/components/AppUserList.vue
+++ front/src/components/AppUserList.vue
@@ -1,22 +1,42 @@
 <template>
-    <div id="app-user-list-container">
-        <table id="app-user-list-table">
+    <div id="app-user-list-container" class="flex justify-center">
+        <table id="app-user-list-table" class="border-collapse shadow-md">
             <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Roles</th>
-                    <th>Actions</th>
+                <tr class="bg-gray-100">
+                    <th class="p-2 border">ID</th>
+                    <th class="p-2 border">Username</th>
+                    <th class="p-2 border">Roles</th>
+                    <th class="p-2 border">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="user in userStore.users" :key="user.id">
-                    <td>{{ user.id }}</td>
-                    <td>{{ user.username }}</td>
-                    <td>{{ user.roles }}</td>
-                    <td>
-                        <button @click="editUser(user.id)">Edit</button>
-                        <button @click="deleteUser(user.id)">Delete</button>
+                    <td class="p-2 border">
+                        <div class="flex justify-center">
+                            <div class="tooltip" v-bind:data-tip="user.id">
+                                {{ truncateData(user.id) }}
+                            </div>
+                        </div>
+                    </td>
+                    <td class="p-2 border">
+                        <div class="flex justify-center">
+                            <div class="tooltip" v-bind:data-tip="user.username">
+                                {{ truncateData(user.username) }}
+                            </div>
+                        </div>
+                    </td>
+                    <td class="p-2 border">
+                        <div class="flex justify-center">
+                            <div class="tooltip" v-bind:data-tip="user.roles">
+                                {{ truncateData(user.roles) }}
+                            </div>
+                        </div>
+                    </td>
+                    <td class="p-2 border">
+                        <div class="flex justify-center">
+                            <button @click="editUser(user.id)">📝</button>
+                            <button @click="deleteUser(user.id)">❌</button>
+                        </div>
                     </td>
                 </tr>
             </tbody>
@@ -34,11 +54,21 @@ onMounted(() => {
     userStore.getUsers();
 });
 
+const truncateData = (data: string) => {
+    if (data.length > 8) {
+        return data.substring(0, 8) + '...';
+    }
+    else {
+        return data;
+    }
+};
+
 const editUser = (id: string) => {
     console.log('Edit user:', id);
 };
 
 const deleteUser = (id: string) => {
-    userStore.deleteUser(id);
+    //userStore.deleteUser(id);
+    console.log('Delete user:', id);
 };
 </script>
